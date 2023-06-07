1cf4c9543cf876f3251105a23ca8a9699ed48554
player01osu <player01osu@gmail.com>
2023-06-07 17:36:21 -0400

    Incremental compilation.

--- Build.hs
+++ Build.hs
@@ -1,8 +1,9 @@
 #!/bin/runghc
 import Script
 import System.Environment
-import System.Process
+import System.Process hiding (runProcess)
 import System.Exit
+import Control.Monad
 import Text.Printf
 import Data.List
 
@@ -16,10 +17,19 @@ srcDir :: String
 srcDir = "src"
 
 src :: IO [String]
-src = outProcessLines "find" [srcDir, "-type", "f"]
+src = outProcessLines "find" [srcDir, "-type", "f", "-name", "*.c"]
 
 obj :: IO [String]
 obj = outProcessLines "find" [objDir, "-type", "f"]
+
+srcs :: IO [String]
+srcs = do
+   src <- src
+   objs <- obj
+   obj <- (foldMap
+      (\file -> outProcessLines "find" $ [file] <> ["-cnewer"] <> [parseObjToSrc file])
+      objs)
+   return $ src \\ map parseObjToSrc obj
 
 makeTarget :: IO ()
 makeTarget = sh $ printf "mkdir -p %s" targetDir
@@ -36,48 +46,50 @@ targetPath = printf "%s/%s" targetDir binName
 cc :: String
 cc = "clang"
 
-cflags :: [String]
-cflags = ["-Wall", "-Wpedantic", "-std=c99"]
+cFlags :: [String]
+cFlags = ["-Wall", "-Wpedantic", "-std=c99"]
 
 linkingFlags :: [String]
 linkingFlags = ["-I/usr/include/SDL2", "-D_REENTRANT", "-L/usr/lib", "-lSDL2"]
 
 makeDir :: IO ()
-makeDir = makeTarget >>= \_ -> makeObj
+makeDir = makeTarget >> makeObj
 
 compileSrc :: IO ()
 compileSrc = do
    makeDir
-   src >>= generateObj
-   src >>= \x -> ((createProcess $ compileCommand x)
-       >>= \(_, _, _, x) -> waitForProcess x)
-   return ()
+   srcs >>= generateObj
+   obj >>= voidProcess . compileCommand
 
+parseObjToSrc :: String -> String
+parseObjToSrc obj
+   | objDir `isPrefixOf` obj = printf "%s%s.c" srcDir (takeWhile (/= '.') src)
+   | otherwise = undefined
+   where Just src = stripPrefix objDir obj
 
 parseSrcToObj :: String -> String
 parseSrcToObj src
-   | srcDir `isPrefixOf` src = obj
+   | srcDir `isPrefixOf` src = printf "%s%s.o" objDir (takeWhile (/= '.') obj)
    | otherwise = undefined
-   where Just obj = stripPrefix srcDir obj
---parseSrcToObj src = printf "%s/%s" objDir $ takeWhile (/= '.') $ tail $ dropWhile (/= '/') src
+   where Just obj = stripPrefix srcDir src
 
 generateObj :: [String] -> IO ()
---generateObj src = map (\x -> (createProcess (proc cc (["-o"] <> [parseSrcToObj x] <> [x]))) >>= \_ -> return ()))
-generateObj = undefined
+generateObj =
+   foldMap (\x -> runProcess cc (["-o"] <> [parseSrcToObj x] <> ["-c"] <> [x]))
 
 compileCommand :: [String] -> CreateProcess
 compileCommand = proc cc . compileArgs
 
 compileArgs :: [String] -> [String]
-compileArgs obj = ["-o", targetPath] <> obj <> cflags <> linkingFlags
+compileArgs obj = obj <> cFlags <> linkingFlags <> ["-o", targetPath]
 
 runBinary :: IO ()
-runBinary = runCommand targetPath >>= (\_ -> return ())
+runBinary = void $ runCommand targetPath
 
 parseArgs :: [String] -> IO ()
 parseArgs [] = compileSrc
 parseArgs ["build"] = compileSrc
-parseArgs ["run"] = compileSrc >>= (\_ -> runBinary)
+parseArgs ["run"] = compileSrc >> runBinary
 
 main :: IO ()
 main = getArgs >>= parseArgs
--- Script.hs
+++ Script.hs
@@ -1,15 +1,21 @@
-module Script (outProcess, outProcessLines, sh) where
+module Script (outProcess, outProcessLines, sh, runProcess, voidProcess) where
 
-import System.Process
+import System.Process hiding (runProcess)
 import System.IO
+import Control.Monad
 
 type ProcessName = String
 type Arguments = [String]
 
 sh :: String -> IO ()
-sh command = do
-   runCommand command >>= \x -> waitForProcess x
-   return ()
+sh command = voidProcess $ shell command
+
+runProcess :: ProcessName -> Arguments -> IO ()
+runProcess process arguments = voidProcess $ proc process arguments
+
+voidProcess :: CreateProcess -> IO ()
+voidProcess process = void $ (createProcess $ process)
+      >>= \(_, _, _, x) -> waitForProcess x
 
 outProcess :: ProcessName -> Arguments -> IO String
 outProcess name arguments = do
