9f9efcd813d4a9c1f25b421e9ec8baa375728e2f
player01osu <player01osu@gmail.com>
2023-06-08 02:00:04 -0400

    Print shell commands in Build.hs.

--- Script.hs
+++ Script.hs
@@ -2,25 +2,34 @@ module Script (outProcess, outProcessLines, sh, runProcess, voidProcess) where
 
 import System.Process hiding (runProcess)
 import System.IO
+import Text.Printf
 import Control.Monad
 
 type ProcessName = String
 type Arguments = [String]
 
 sh :: String -> IO ()
-sh command = voidProcess $ shell command
+sh command = (voidProcess $ shell command)
+
+printProc :: CreateProcess -> IO ()
+printProc proc = putStrLn $ stringProcess (cmdspec proc)
+   where
+      stringProcess :: CmdSpec -> String
+      stringProcess (ShellCommand command) = command
+      stringProcess (RawCommand filePath command) = printf "%s %s" filePath $ unwords command
 
 runProcess :: ProcessName -> Arguments -> IO ()
-runProcess process arguments = voidProcess $ proc process arguments
+runProcess process arguments = (voidProcess $ proc process arguments)
 
 voidProcess :: CreateProcess -> IO ()
-voidProcess process = void $ (createProcess $ process)
-      >>= \(_, _, _, x) -> waitForProcess x
+voidProcess process = (void $ (createProcess $ process)
+      >>= \(_, _, _, x) -> waitForProcess x) >> printProc process
 
 outProcess :: ProcessName -> Arguments -> IO String
 outProcess name arguments = do
    (_, Just hout, _, _) <-
       createProcess (proc name arguments) { std_out = CreatePipe }
+   printProc (proc name arguments)
    hGetContents hout
 
 outProcessLines :: ProcessName -> Arguments -> IO [String]
