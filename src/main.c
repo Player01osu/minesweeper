b2e6fe630760aa0905a77eaf483cba1d41dfd31e
player01osu <player01osu@gmail.com>
2023-06-07 20:57:31 -0400

    Toggle tiles by clicking.

--- src/main.c
+++ src/main.c
@@ -10,8 +10,21 @@
 #define WIDTH 1280
 #define HEIGHT 720
 
-const Uint32 SQUARE_COLOR = 0x111111;
 const Uint32 BACKGROUND_COLOR = 0x888888;
+const Uint32 TILE_UNCLICKED_COLOR = 0x111111;
+const Uint32 TILE_CLICKED_COLOR = 0xFFFFFF;
+
+#define COLS 10
+#define ROWS 10
+const Uint32 PAD_INNER = 5;
+const Uint32 PAD_OUTER = 30;
+
+typedef struct {
+	SDL_Rect rect;
+	bool clicked;
+} Tile;
+
+Tile tiles[COLS][ROWS];
 
 typedef struct {
 	SDL_Window *window;
@@ -64,6 +77,13 @@ void u32_color(Uint32 rgb, Uint8 *r, Uint8 *g, Uint8 *b)
 	*b = color.b;
 }
 
+void set_render_color_u32(Ctx ctx, Uint32 rgb, Uint8 alpha)
+{
+	Uint8 r, g, b;
+	u32_color(rgb, &r, &g, &b);
+	SDL_SetRenderDrawColor(ctx.renderer, r, g, b, alpha);
+}
+
 SDL_Rect rect_new(int x, int y, int w, int h)
 {
 	SDL_Rect rect = {
@@ -75,56 +95,105 @@ SDL_Rect rect_new(int x, int y, int w, int h)
 	return rect;
 }
 
-const Uint32 COLS = 10;
-const Uint32 ROWS = 10;
-const Uint32 PAD_INNER = 5;
-const Uint32 PAD_OUTER = 30;
+typedef struct {
+	Uint32 rect_width;
+	Uint32 rect_height;
+} GridCalc;
+
+GridCalc grid_calc(void)
+{
+	Uint32 min_canvas = WIDTH > HEIGHT ? HEIGHT : WIDTH;
+
+	Uint32 width_adjusted = min_canvas - PAD_OUTER - PAD_INNER * COLS;
+	Uint32 height_adjusted = min_canvas - PAD_OUTER - PAD_INNER * ROWS;
+
+	Uint32 rect_width = width_adjusted / COLS;
+	Uint32 rect_height = height_adjusted / ROWS;
+
+	GridCalc calc = {
+		rect_width,
+		rect_height
+	};
+	return calc;
+}
+
+SDL_Rect grid_rect(size_t row, size_t col)
+{
+	GridCalc calc = grid_calc();
+	Uint32 rect_width = calc.rect_width;
+	Uint32 rect_height = calc.rect_height;
+
+	Uint32 x = (WIDTH / 2 - PAD_OUTER) / 2 + col * (rect_width + PAD_INNER);
+	Uint32 y = (PAD_OUTER) / 2 + row * (rect_height + PAD_INNER);
+
+	return rect_new(x, y, rect_width, rect_height);
+}
 
 void create_grid(Ctx ctx)
 {
-	Uint8 r, g, b;
-	u32_color(SQUARE_COLOR, &r, &g, &b);
-
-	SDL_SetRenderDrawColor(ctx.renderer, r, g, b, SDL_ALPHA_OPAQUE);
-
-	Uint32 width = WIDTH;
-	Uint32 height = HEIGHT;
-
-	Uint32 min_canvas = width > height ? height : width;
-
-	Uint32 pad_outer = PAD_OUTER;
-	Uint32 pad_inner = PAD_INNER;
-
-	Uint32 rows = ROWS;
-	Uint32 cols = COLS;
-
-	Uint32 width_adjusted = min_canvas - pad_outer - pad_inner * cols;
-	Uint32 height_adjusted = min_canvas - pad_outer - pad_inner * rows;
-
-	Uint32 rect_width = width_adjusted / cols;
-	Uint32 rect_height = height_adjusted / rows;
-
-	for (size_t row = 0; row < rows; ++row) {
-		for (size_t col = 0; col < cols; ++col) {
-			SDL_Rect rect = rect_new(
-				(width / 2 - pad_outer) / 2 + col * (rect_width + pad_inner),
-				(pad_outer) / 2 + row * (rect_height + pad_inner),
-				rect_width,
-				rect_height
-			);
-			SDL_RenderFillRect(ctx.renderer, &rect);
+	for (size_t row = 0; row < ROWS; ++row) {
+		for (size_t col = 0; col < COLS; ++col) {
+			Tile tile = {
+				.rect = grid_rect(row, col),
+				.clicked = false,
+			};
+			tiles[row][col] = tile;
 		}
 	}
+}
 
+void draw_grid(Ctx ctx)
+{
+	for (size_t row = 0; row < ROWS; ++row) {
+		for (size_t col = 0; col < COLS; ++col) {
+			Tile tile = tiles[row][col];
+			if (tile.clicked) {
+				set_render_color_u32(ctx, TILE_CLICKED_COLOR, SDL_ALPHA_OPAQUE);
+			} else {
+				set_render_color_u32(ctx, TILE_UNCLICKED_COLOR, SDL_ALPHA_OPAQUE);
+			}
+			SDL_RenderFillRect(ctx.renderer, &tile.rect);
+		}
+	}
+}
+
+void toggle_tile(Ctx ctx, Uint32 x, Uint32 y)
+{
+	GridCalc calc = grid_calc();
+	Uint32 rect_width = calc.rect_width;
+	Uint32 rect_height = calc.rect_height;
+	size_t col = (x - (WIDTH / 2 - PAD_OUTER) / 2) / (rect_width + PAD_INNER);
+	size_t row = (y - (PAD_OUTER) / 2) / (rect_height + PAD_INNER);
+
+	if (col < 0 || col > COLS - 1 || row < 0 || row > ROWS - 1)
+		return;
+
+	Tile *tile = &tiles[row][col];
+	tile->clicked = !tile->clicked;
 }
 
 void clear_background(Ctx ctx)
 {
-	Uint8 r, g, b;
-	u32_color(BACKGROUND_COLOR, &r, &g, &b);
-
-	SDL_SetRenderDrawColor(ctx.renderer, r, g, b, SDL_ALPHA_OPAQUE);
+	set_render_color_u32(ctx, BACKGROUND_COLOR, SDL_ALPHA_OPAQUE);
 	SDL_RenderClear(ctx.renderer);
+}
+
+void capframerate(long *then, float *remainder)
+{
+	long wait, frameTime;
+
+	wait = 16 + *remainder;
+	*remainder -= (int)*remainder;
+	frameTime = SDL_GetTicks64() - *then;
+	wait -= frameTime;
+
+	if (wait < 1) {
+		wait = 1;
+	}
+
+	SDL_Delay(wait);
+	*remainder += 0.667;
+	*then = SDL_GetTicks64();
 }
 
 int main(int argc, char **argv)
@@ -132,7 +201,12 @@ int main(int argc, char **argv)
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
 		printf("error initializing SDL: %s\n", SDL_GetError());
 	}
+	/* control framerate */
+	long then;
+	float remainder;
+
 	Ctx ctx = ctx_new();
+	create_grid(ctx);
 
 	bool running = true;
 	while (running) {
@@ -150,12 +224,21 @@ int main(int argc, char **argv)
 			case SDL_QUIT:
 				running = false;
 				break;
+			case SDL_MOUSEBUTTONDOWN:
+				switch (event.button.button) {
+				case SDL_BUTTON_LEFT: {
+					Uint32 x = event.button.x;
+					Uint32 y = event.button.y;
+					toggle_tile(ctx, x, y);
+				} break;
+				}
 			}
 		}
 
-		create_grid(ctx);
+		draw_grid(ctx);
 
 		SDL_RenderPresent(ctx.renderer);
+		capframerate(&then, &remainder);
 	}
 
 	destroy_ctx(ctx);
