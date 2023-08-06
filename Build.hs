808c834ad52c1ccc100cbc3c8f6fbe7f91a8884f
player01osu <player01osu@gmail.com>
2023-08-06 13:44:10 +0900

    More extensible flags, subcommands, and release and debug builds.

--- Build.hs
+++ Build.hs
@@ -4,8 +4,10 @@ import System.Environment
 import System.Process hiding (runProcess)
 import System.Exit
 import Control.Monad
+import Control.Exception
 import Text.Printf
 import Data.List
+import Data.Either
 
 targetDir :: String
 targetDir = "target"
@@ -22,70 +24,87 @@ src = outProcessLines "find" [srcDir, "-type", "f", "-name", "*.c"]
 obj :: IO [String]
 obj = outProcessLines "find" [objDir, "-type", "f"]
 
-srcs :: IO [String]
-srcs = do
+srcs :: String -> IO [String]
+srcs objDir = do
    src <- src
    objs <- obj
    obj <- (foldMap
-      (\file -> outProcessLines "find" $ [file] <> ["-cnewer"] <> [parseObjToSrc file])
+      --(\file -> outProcessLines "find" $ [file] <> ["-cnewer"] <> [parseObjToSrc file objDir])
+      (\file -> outProcessLines "find" $ [file] <> ["-cnewer"] <> [parseObjToSrc file objDir])
       objs)
-   return $ src \\ map parseObjToSrc obj
+   return $ src \\ map (\x -> parseObjToSrc x objDir) obj
 
-makeTarget :: IO ()
-makeTarget = sh $ printf "mkdir -p %s" targetDir
-
-makeObj :: IO ()
-makeObj = sh $ printf "mkdir -p %s" objDir
+mkDir dir = sh $ printf "mkdir -p %s" dir
 
 binName :: String
 binName = "minesweeper"
 
-targetPath :: String
-targetPath = printf "%s/%s" targetDir binName
+targetPath :: String -> String
+targetPath targetDir = printf "%s/%s" targetDir binName
 
 cc :: String
 cc = "clang"
 
 cFlags :: [String]
-cFlags = ["-Wall", "-Wpedantic", "-std=c99", "-Wextra", "-fno-omit-frame-pointer", "-g3"]
+cFlags = ["-Wall", "-Wpedantic", "-std=c99", "-Wextra", "-fno-omit-frame-pointer"]
 -- "-fsanitize=address"
 
 linkingFlags :: [String]
 linkingFlags = ["-I/usr/include/SDL2", "-D_REENTRANT", "-L/usr/lib", "-lSDL2", "-lSDL2_ttf"]
 
-makeDir :: IO ()
-makeDir = makeTarget >> makeObj
+makeDir :: String -> String -> IO ()
+makeDir objDir targetDir = mkDir objDir >> mkDir targetDir
 
-compileSrc :: IO ()
-compileSrc = do
-   makeDir
-   srcs >>= generateObj
-   obj >>= voidProcess . compileCommand
+compileSrc :: [Flag] -> IO ()
+compileSrc flag = do
+   makeDir objDir targetDir
+   srcs objDir >>= generateObj flags objDir
+   obj >>= voidProcess . compileCommand flags targetDir
+   where
+      flags = flags' flag
+      flags' []               = ["-g3"]
+      flags' (FlagDebug:xs)   = ["-g3"]
+      flags' (FlagRelease:xs) = ["-o2", "-ffast-math"]
 
-parseObjToSrc :: String -> String
-parseObjToSrc obj
+      objDir = objDir' flag
+      objDir' []               = "obj/debug"
+      objDir' (FlagDebug:xs)   = "obj/debug"
+      objDir' (FlagRelease:xs) = "obj/release"
+
+      targetDir = targetDir' flag
+      targetDir' []               = "target/debug"
+      targetDir' (FlagDebug:xs)   = "target/debug"
+      targetDir' (FlagRelease:xs) = "target/release"
+
+parseObjToSrc :: String -> String -> String
+parseObjToSrc [] objDir = ""
+parseObjToSrc obj objDir
    | objDir `isPrefixOf` obj = printf "%s%s.c" srcDir (takeWhile (/= '.') src)
    | otherwise = undefined
    where Just src = stripPrefix objDir obj
 
-parseSrcToObj :: String -> String
-parseSrcToObj src
+parseSrcToObj :: String -> String -> String
+parseSrcToObj src objDir
    | srcDir `isPrefixOf` src = printf "%s%s.o" objDir (takeWhile (/= '.') obj)
    | otherwise = undefined
    where Just obj = stripPrefix srcDir src
 
-generateObj :: [String] -> IO ()
-generateObj =
-   foldMap (\x -> runProcess cc (["-o"] <> [parseSrcToObj x] <> ["-c"] <> [x]))
+generateObj :: [String] -> String -> [String] -> IO ()
+generateObj flags objDir =
+   foldMap (\x -> runProcess cc (["-o"] <> [parseSrcToObj x objDir] <> cFlags <> flags <> ["-c"] <> [x]))
 
-compileCommand :: [String] -> CreateProcess
-compileCommand = proc cc . compileArgs
+compileCommand :: [String] -> String -> [String] -> CreateProcess
+compileCommand flags targetDir = proc cc . (compileArgs flags targetDir)
 
-compileArgs :: [String] -> [String]
-compileArgs obj = obj <> cFlags <> linkingFlags <> ["-o", targetPath]
+compileArgs :: [String] -> String -> [String] -> [String]
+compileArgs flags targetDir obj = obj <> flags <> cFlags <> linkingFlags <> ["-o", targetPath targetDir]
 
-runBinary :: IO ()
-runBinary = void $ runCommand targetPath
+runBinary :: [Flag] -> IO ()
+runBinary flags = void $ runCommand $ targetPath $ targetDir flags
+   where
+      targetDir []               = "target/debug"
+      targetDir (FlagDebug:xs)   = "target/debug"
+      targetDir (FlagRelease:xs) = "target/release"
 
 cleanWorkingDir :: IO ()
 cleanWorkingDir = runProcess "rm" ["-rf", targetDir, objDir]
@@ -93,19 +112,73 @@ cleanWorkingDir = runProcess "rm" ["-rf", targetDir, objDir]
 compileBuild :: IO ()
 compileBuild = runProcess "ghc" ["-no-keep-hi-files", "-no-keep-o-files", "Build.hs"]
 
-invalidArg :: [String] -> IO ()
-invalidArg = putStrLn . printf "Invalid argument: %s" . unwords
+data SubCmd = SubCmdBuild
+            | SubCmdRun
+            | SubCmdClean
+            | SubCmdCompileBuild
+            | SubCmdForce
+            | SubCmdForceRun
+
+data Flag = FlagDebug
+          | FlagRelease
 
 parseArgs :: [String] -> IO ()
-parseArgs [] = compileSrc
-parseArgs args
-   | args `elem` [["build"], ["b"]]          = compileSrc
-   | args `elem` [["run"], ["r"]]            = compileSrc >> runBinary
-   | args `elem` [["clean"], ["c"]]          = cleanWorkingDir
-   | args `elem` [["compile-build"], ["cb"]] = compileBuild
-   | args `elem` [["force"], ["f"]]          = cleanWorkingDir >> compileSrc
-   | args `elem` [["force-run"], ["fr"]]     = cleanWorkingDir >> compileSrc >> runBinary
-   | otherwise                               = invalidArg args
+parseArgs [] = compileSrc [FlagDebug]
+parseArgs args = executeSubCommand $ pairArgs subCommand flags
+   where
+      executeSubCommand :: Either (SubCmd, [Flag]) String -> IO ()
+      executeSubCommand (Left (SubCmdBuild,        flags)) = compileSrc flags
+      executeSubCommand (Left (SubCmdRun,          flags)) = compileSrc flags >> runBinary flags
+      executeSubCommand (Left (SubCmdClean,        flags)) = cleanWorkingDir
+      executeSubCommand (Left (SubCmdCompileBuild, flags)) = compileBuild
+      executeSubCommand (Left (SubCmdForce,        flags)) = cleanWorkingDir >> compileSrc flags
+      executeSubCommand (Left (SubCmdForceRun,     flags)) = cleanWorkingDir >> compileSrc flags >> runBinary flags
+      executeSubCommand (Right msg)                        = putStrLn msg >> die
+
+      pairArgs :: Either SubCmd String -> Either [Flag] String -> Either (SubCmd, [Flag]) String
+      pairArgs (Right msg) _              = Right msg
+      pairArgs _ (Right msg)              = Right msg
+      pairArgs (Left subCmd) (Left flags) = Left (subCmd, flags)
+
+      matchSubCommand :: String -> Either SubCmd String
+      matchSubCommand cmd
+         | cmd `elem` ["build", "b"]          = Left SubCmdBuild
+         | cmd `elem` ["run", "r"]            = Left SubCmdRun
+         | cmd `elem` ["clean", "c"]          = Left SubCmdClean
+         | cmd `elem` ["compile-build", "cb"] = Left SubCmdCompileBuild
+         | cmd `elem` ["force", "f"]          = Left SubCmdForce
+         | cmd `elem` ["force-run", "fr"]     = Left SubCmdForceRun
+         | otherwise                          = Right $ printf "Invalid subcommand: %s" cmd
+
+      matchFlag :: String -> Either Flag String
+      matchFlag flag
+         | flag `elem` ["--debug"] = Left FlagDebug
+         | flag `elem` ["--release"]   = Left FlagRelease
+         | otherwise                   = Right $ printf "Invalid flag: %s" flag
+
+      rawSubCmd :: [String]
+      rawFlags :: [String]
+      (rawFlags, rawSubCmd) = partition (isPrefixOf "--") args
+
+      subCommand :: Either SubCmd String
+      subCommand = parseSubCommand rawSubCmd
+
+      flags :: Either [Flag] String
+      flags
+         | flagErrors == "" = Left $ lefts mappedFlags
+         | otherwise        = Right flagErrors
+
+      flagErrors = unwords $ rights mappedFlags
+
+      mappedFlags :: [Either Flag String]
+      mappedFlags = map matchFlag rawFlags
+
+      parseSubCommand :: [String] -> Either SubCmd String
+      parseSubCommand [] = Left SubCmdBuild
+      parseSubCommand [x] = matchSubCommand x
+      parseSubCommand _ = Right "Too many subcommands provided"
+
+      die = exitWith (ExitFailure 1)
 
 main :: IO ()
 main = getArgs >>= parseArgs
